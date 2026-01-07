8e9625ab37e36fd1ff0a78588a35000da833344b
tusharshah21 <tusharkumarshah14394@gmail.com>
2026-01-07 15:37:54 +0530

    implemented eslint for the repo

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Acquisitions Service is up and running!');
+  res.status(200).send('Acquisitions Service is up and running!');
 });
 
 export default app;
