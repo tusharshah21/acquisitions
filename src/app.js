c8fded00bb3ae975ff7d444d0c876a10da6b1731
tusharshah21 <tusharkumarshah14394@gmail.com>
2026-01-08 13:43:30 +0530

    added model, helmet and winston

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+
 const app = express();
 
+app.use(helmet());
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions Service!');
+
   res.status(200).send('Acquisitions Service is up and running!');
 });
 
